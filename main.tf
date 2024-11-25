e6b50254b5b628ad1acd2d576cf3d06688a66973
Hasara Dulanjana <hasara.dulanjana.16@gmail.com>
2024-11-25 06:48:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,34 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 
 variable "servers" {
   description = "List of server configurations"
-  type        = list(object({
+  type = list(object({
     name              = string
     instance_type     = string
     ami_id            = string
-    security_groups   = list(string)
+    security_group_id = string
     key_name          = string
+    vpc_id            = string
+    subnet_id         = string
   }))
 }
 
 resource "aws_instance" "ec2_instance" {
-  for_each = { for idx, server in var.servers : server.name => server }
+  for_each = { for server in var.servers : server.name => server }
 
   ami                    = each.value.ami_id
   instance_type          = each.value.instance_type
   key_name               = each.value.key_name
-  security_group_ids     = each.value.security_groups
+  security_group_ids     = [each.value.security_group_id]
+  subnet_id              = each.value.subnet_id
+
   tags = {
     Name = each.value.name
   }
 }
 
 output "instance_ips" {
-  value = aws_instance.ec2_instance[*].public_ip
+  value = { for name, instance in aws_instance.ec2_instance : name => instance.public_ip }
 }
