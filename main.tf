9744897e495bb39c701653309afdaf45f6bd60cb
Hasara Dulanjana <hasara.dulanjana.16@gmail.com>
2024-11-25 07:26:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,20 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+# Load the JSON file containing the server configurations
+locals {
+  servers = jsondecode(file("config.json")).servers
+}
+
 resource "aws_instance" "ec2_instance" {
-  for_each = { for server in var.servers : server.name => server }
+  for_each = { for server in local.servers : server.name => server }
 
-  ami                    = each.value.ami_id
-  instance_type          = each.value.instance_type
-  key_name               = each.value.key_name
-  vpc_security_group_ids = [each.value.security_group_id]
-  subnet_id              = each.value.subnet_id
-
-  associate_public_ip_address = each.value.assign_public_ip  # Conditional public IP assignment
+  ami                         = each.value.ami_id
+  instance_type               = each.value.instance_type
+  key_name                    = each.value.key_name
+  security_groups             = [each.value.security_group_id]  # Changed this line
+  subnet_id                   = each.value.subnet_id
+  associate_public_ip_address = each.value.assign_public_ip
 
   tags = {
     Name = each.value.name
