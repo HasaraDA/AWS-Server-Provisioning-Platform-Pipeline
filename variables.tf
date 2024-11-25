34062d0eeaa918f88a0944502794da77eae611e4
Hasara Dulanjana <hasara.dulanjana.16@gmail.com>
2024-11-25 06:48:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "servers" {
   description = "List of server configurations"
   type = list(object({
-    name            = string
-    instance_type   = string
-    ami_id          = string
-    security_groups = list(string)
-    key_name        = string
+    name              = string
+    instance_type     = string
+    ami_id            = string
+    security_group_id = string
+    key_name          = string
+    vpc_id            = string
+    subnet_id         = string
   }))
 }
